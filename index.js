e201cbd13fe39ba1748b84199f2728abba572817
Aditi Ohri <didiohri@gmail.com>
2019-07-16 02:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = 'Lets talk about space.';
 
-var conversation = `Let\s talk about ${topic}.`;
+var conversation = `Let's talk about ${topic}.`;
